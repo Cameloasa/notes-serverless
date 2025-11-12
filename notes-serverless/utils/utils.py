331fd039350187e92284a6934e07c9eaa1a27061
Camelia Ciuca <camelia1414@gmail.com>
2025-11-12 12:38:48 +0100

    utils

---
+++ notes-serverless/utils/utils.py
@@ -0,0 +1,17 @@
+import uuid
+from datetime import datetime, timezone
+
+def generate_note_item(username, title,text):
+    # Generate a new note item wwith a unique ID and timestamp
+
+    note_id = str(uuid.uuid4())
+    now =  datetime.now(timezone.utc).isoformat()
+
+    return {
+        'id': note_id,
+        'username': username,
+        'title': title,
+        'text': text,
+        'createdAt': now,
+        'modifiedAt': now
+    }
